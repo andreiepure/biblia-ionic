38cf679ffa7821346bc472799a5e9901553b01a4
Andrei Epure <epure.andrei@gmail.com>
2017-01-19 12:20:12 +0100

    added dummy books to simulate the generated ones

--- src/providers/bible-service.ts
+++ src/providers/bible-service.ts
@@ -2,6 +2,20 @@ import { Injectable } from '@angular/core';
 import { Http } from '@angular/http';
 import 'rxjs/add/operator/map';
 
+import { IVersetLink } from '../models/verset-link.interface';
+import { IBook } from "../models/book.interface";
+
+import { Dt } from './books/Dt'
+import { Fa } from './books/Fa'
+import { Fc } from './books/Fc'
+import { Ies } from './books/Ies'
+import { In } from './books/In'
+import { Lc } from './books/Lc'
+import { Lv } from './books/Lv'
+import { Mc } from './books/Mc'
+import { Mt } from './books/Mt'
+import { Nm } from './books/Nm'
+
 /*
   Generated class for the BibleData provider.
 
@@ -11,11 +25,53 @@ import 'rxjs/add/operator/map';
 @Injectable()
 export class BibleService {
 
+  private oldTestament : IBook[] = [
+    Fc, Ies, Dt, Lv, Nm
+  ];
+
+  private newTestament : IBook[] = [
+    Mt, Mc, Lc, In, Fa, Nm
+  ];
+
   constructor(public http: Http) {
     console.log('Hello BibleData Provider');
   }
 
   print(message: string): void {
     console.log('Received the message: ' + message)
+  }
+
+  GetBooks(testament: string): IBook[] {
+    var books;
+    if (testament == "VT") {
+      books = this.oldTestament;
+    }
+    else if (testament == "NT") {
+      books = this.newTestament;
+    }
+    else {
+      throw ("Unknown testament");
+    }
+
+    return books;
+    //return books.map((book: IBook) => { return book.longName });
+  }
+
+  GetChapters(bookShortName: string) {
+
+  }
+
+  GetChapterVersets(bookShortName: string, chapterIndex: number) {
+
+  }
+
+  GetLinkVersets(versetLink: IVersetLink) {
+
+  }
+
+  GetVerset(bookShortName: string,
+    chapterIndex: number,
+    versetIndex: number) {
+
   }
 }
